f26868e12df682be8c54b094e1c289015844422e
MaBoRo <vlad.tishyn@gmail.com>
2021-10-05 03:19:54 +0300

    NotificationService.kt create

---
+++ app/src/main/java/com/example/exoplayer/service/NotificationService.kt
@@ -0,0 +1,91 @@
+package com.example.exoplayer.service
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.example.exoplayer.R
+import com.example.exoplayer.resource.VideoCredentials.CHANNEL_ID
+import com.example.exoplayer.ui.activities.ExoPlayerActivity
+
+class NotificationService : Service() {
+    companion object {
+        fun startService(context: Context, message: String) {
+            val startIntent = Intent(context, NotificationService::class.java)
+            startIntent.putExtra("player_status", message)
+            ContextCompat.startForegroundService(context, startIntent)
+        }
+
+        fun stopService(context: Context) {
+            val stopIntent = Intent(context, NotificationService::class.java)
+            context.stopService(stopIntent)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val contentIntent = PendingIntent.getActivity(
+            this, 0,
+            Intent(this, ExoPlayerActivity::class.java), PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        //do heavy work on a background thread
+        val status = intent?.getStringExtra("player_status")
+        createNotificationChannel()
+//        val notificationIntent = Intent(this, ExoPlayerActivity::class.java)
+//        val pendingIntent = PendingIntent.getActivity(
+//            this,
+//            0, notificationIntent, 0
+//        )
+        val notification: Notification
+        if (status == "true") {
+            notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setSmallIcon(R.drawable.exo_notification_small_icon)
+                // Add media control buttons that invoke intents in your media service
+                .addAction(R.drawable.exo_icon_previous, "Previous", null) // #0
+                .addAction(R.drawable.exo_icon_pause, "Pause", null) // #1
+                .addAction(R.drawable.exo_icon_next, "Next", null) // #2
+                // Apply the media style template
+                .setStyle(androidx.media.app.NotificationCompat.MediaStyle())
+                .setContentTitle("Simple Title")
+                .setContentText("Simple text")
+                .setContentIntent(contentIntent)
+                .build()
+            startForeground(1, notification)
+        } else {
+            notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setSmallIcon(R.drawable.exo_notification_small_icon)
+                // Add media control buttons that invoke intents in your media service
+                .addAction(R.drawable.exo_icon_previous, "Previous", null) // #0
+                .addAction(R.drawable.exo_icon_play, "Play", null) // #1
+                .addAction(R.drawable.exo_icon_next, "Next", null) // #2
+                // Apply the media style template
+                .setStyle(androidx.media.app.NotificationCompat.MediaStyle())
+                .setContentTitle("Simple Title")
+                .setContentText("Simple text")
+                .setContentIntent(contentIntent)
+                .build()
+            startForeground(1, notification)
+        }
+        //stopSelf();
+        return START_NOT_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID, "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager!!.createNotificationChannel(serviceChannel)
+        }
+    }
+}
--- app/src/main/java/com/example/exoplayer/ui/activities/ExoPlayerActivity.kt
+++ app/src/main/java/com/example/exoplayer/ui/activities/ExoPlayerActivity.kt
@@ -13,6 +13,7 @@ import com.example.exoplayer.R
 import com.example.exoplayer.databinding.ExoplayerActivityBinding
 import com.example.exoplayer.resource.VideoCredentials.CHANNEL_ID
 import com.example.exoplayer.resource.VideoCredentials.CHANNEL_NAME_1
+import com.example.exoplayer.service.NotificationService
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
@@ -21,7 +22,6 @@ import com.google.android.exoplayer2.SimpleExoPlayer
 class ExoPlayerActivity : AppCompatActivity(), Player.Listener {
     private lateinit var binding: ExoplayerActivityBinding
     private lateinit var mainPlayer: SimpleExoPlayer
-    private lateinit var mNotificationManagerCompat: NotificationManagerCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +30,6 @@ class ExoPlayerActivity : AppCompatActivity(), Player.Listener {
         val view = binding.root
         setContentView(view)
 
-        initNotification()
 
         val activityIntent = intent
         val url = activityIntent.getStringExtra("url")
@@ -61,79 +60,24 @@ class ExoPlayerActivity : AppCompatActivity(), Player.Listener {
 
     }
 
-    private fun initNotification() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_NAME_1,
-                NotificationManager.IMPORTANCE_DEFAULT
-            ).apply {
-                lightColor = Color.GREEN
-                enableLights(true)
-            }
-            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            manager.createNotificationChannel(channel)
-        }
-    }
-
     override fun onIsPlayingChanged(isPlaying: Boolean) {
-        val contentIntent = PendingIntent.getActivity(
-            this, 0,
-            Intent(this, ExoPlayerActivity::class.java), PendingIntent.FLAG_UPDATE_CURRENT
-        )
         if (isPlaying) {
-            mNotificationManagerCompat = NotificationManagerCompat.from(this)
-            val notification = this.let { it1 ->
-                NotificationCompat.Builder(it1, CHANNEL_ID)
-                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                    .setSmallIcon(R.drawable.exo_notification_small_icon)
-                    // Add media control buttons that invoke intents in your media service
-                    .addAction(R.drawable.exo_icon_previous, "Previous", null) // #0
-                    .addAction(R.drawable.exo_icon_pause, "Pause", null) // #1
-                    .addAction(R.drawable.exo_icon_next, "Next", null) // #2
-                    // Apply the media style template
-                    .setStyle(androidx.media.app.NotificationCompat.MediaStyle())
-                    .setContentTitle("Simple Title")
-                    .setContentText("Simple text")
-                    .setContentIntent(contentIntent)
-                    .build()
-            }
-            mNotificationManagerCompat.notify(1, notification)
+            NotificationService.startService(this, "true")
         } else {
-            val notificationIntent = Intent(this, ExoPlayerActivity::class.java)
-            notificationIntent.addCategory(Intent.CATEGORY_LAUNCHER)
-            notificationIntent.action = Intent.ACTION_MAIN
-            notificationIntent.flags =
-                Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-            mNotificationManagerCompat = NotificationManagerCompat.from(this)
-            val notification = this.let { it1 ->
-                NotificationCompat.Builder(it1, CHANNEL_ID)
-                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                    .setSmallIcon(R.drawable.exo_notification_small_icon)
-                    // Add media control buttons that invoke intents in your media service
-                    .addAction(R.drawable.exo_icon_previous, "Previous", null) // #0
-                    .addAction(R.drawable.exo_icon_play, "Play", null) // #1
-                    .addAction(R.drawable.exo_icon_next, "Next", null) // #2
-                    // Apply the media style template
-                    .setStyle(androidx.media.app.NotificationCompat.MediaStyle())
-                    .setContentTitle("Simple Title")
-                    .setContentText("Simple text")
-                    .setContentIntent(contentIntent)
-                    .build()
-            }
-            mNotificationManagerCompat.notify(1, notification)
+            NotificationService.startService(this, "false")
         }
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
         val intent = Intent(this, VideoListActivity::class.java)
+        NotificationService.stopService(this)
         startActivity(intent)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mNotificationManagerCompat.cancelAll()
+        NotificationService.stopService(this)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
